31f5646d0de58c183f0cdbb4ac54aac41264480d
Pauline Tayoto <paulinetayoto@gmail.com>
2018-09-12 14:17:01 +0800

    Initial commit

---
+++ app/src/main/java/com/tayoto/pauline/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tayoto.pauline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.styles_material_design);
+    }
+}
